056c2f0e26b97c61ae0408ef64bae6071a8574e6
tshemsedinov <timur.shemsedinov@gmail.com>
2018-02-22 02:58:17 +0200

    Implement delta property

---
+++ JavaScript/6-delta.js
@@ -0,0 +1,78 @@
+'use strict';
+
+function Transaction() {}
+
+Transaction.start = (data) => {
+  console.log('\nstart transaction');
+  let delta = {};
+
+  const methods = {
+    commit: () => {
+      console.log('\ncommit transaction');
+      Object.assign(data, delta);
+      delta = {};
+    },
+    rollback: () => {
+      console.log('\nrollback transaction');
+      delta = {};
+    },
+    clone: () => {
+      console.log('\nclone transaction');
+      const cloned = Transaction.start(data);
+      Object.assign(cloned.delta, delta);
+      return cloned;
+    }
+  };
+
+  return new Proxy(data, {
+    get(target, key) {
+      if (key === 'delta') return delta;
+      if (methods.hasOwnProperty(key)) return methods[key];
+      if (delta.hasOwnProperty(key)) return delta[key];
+      return target[key];
+    },
+    getOwnPropertyDescriptor: (target, key) => (
+      Object.getOwnPropertyDescriptor(
+        delta.hasOwnProperty(key) ? delta : target, key
+      )
+    ),
+    ownKeys() {
+      const changes = Object.keys(delta);
+      const keys = Object.keys(data).concat(changes);
+      return keys.filter((x, i, a) => a.indexOf(x) === i);
+    },
+    set(target, key, val) {
+      console.log('set', key, val);
+      if (target[key] === val) delete delta[key];
+      else delta[key] = val;
+      return true;
+    }
+  });
+};
+
+// Usage
+
+const data = { name: 'Marcus Aurelius', born: 121 };
+
+const transaction = Transaction.start(data);
+console.dir({ data });
+
+transaction.name = 'Mao Zedong';
+transaction.born = 1893;
+transaction.city = 'Shaoshan';
+console.dir({ transaction });
+console.dir({ delta: transaction.delta });
+
+transaction.commit();
+console.dir({ data });
+console.dir({ transaction });
+console.dir({ delta: transaction.delta });
+
+transaction.born = 1976;
+console.dir({ transaction });
+console.dir({ delta: transaction.delta });
+
+transaction.rollback();
+console.dir({ data });
+console.dir({ transaction });
+console.dir({ delta: transaction.delta });
